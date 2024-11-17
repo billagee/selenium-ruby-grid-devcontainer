c6cc1236d0917ca03f14951721be3e5c850fb866
Bill Agee <billagee@gmail.com>
2024-11-17 00:25:22 -0800

    More tweaks

--- spec/remote_webdriver_spec.rb
+++ spec/remote_webdriver_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'selenium-webdriver'
 
 RSpec.describe 'Remote WebDriver' do
+  # TODO: Borrow more example code from
+  # https://github.com/SeleniumHQ/seleniumhq.github.io/blob/trunk/examples/ruby/spec/drivers/remote_webdriver_spec.rb
   let(:selenium_grid_url) { "http://#{ENV['GRIDHOST']}:#{ENV['GRIDPORT']}/wd/hub" }
   let(:options) { Selenium::WebDriver::Options.chrome(args: ["window-size=1000,800"]) }
   let(:driver) { Selenium::WebDriver.for(:remote, url: selenium_grid_url, capabilities: options) }
@@ -10,7 +12,7 @@ RSpec.describe 'Remote WebDriver' do
     driver.quit
   end
 
-  it 'navigates to Google and prints the page title' do
+  it 'navigates to Google and checks the page title' do
     driver.navigate.to "https://www.google.com"
     expect(driver.title).to eq("Google")
   end
